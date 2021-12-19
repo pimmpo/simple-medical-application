eb75b73fc66a3f0aec200e184ae2874540df3215
pimmpo <whbrat@gmail.com>
2021-12-20 01:06:25 +0300

    Initialization project, settings environment, create connection with database

---
+++ src/main/java/com/pimmpo/simplemedicalapplication/SimpleMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.pimmpo.simplemedicalapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pimmpo/simplemedicalapplication/SimpleMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimmpo.simplemedicalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
